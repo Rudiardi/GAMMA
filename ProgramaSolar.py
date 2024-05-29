fe107b8977bc868d78440f53d1316a2f5df56ef9
Rudiardi <rudi.junior@hotmail.com>
2024-05-28 21:55:31 -0300

    teste - ok

--- ProgramaSolar.py
+++ ProgramaSolar.py
@@ -2,3 +2,23 @@
 from tkinter import * # biblioteca gráfica TKinter pip install tk
 from tkinter import ttk
 from docx import Document #biblioteca que importa documentos
+from openpyxl import load_workbook #Biblioteca para abrir o excel
+
+#Criação de dicionário para a substituição das variaveis
+def editword():
+    #aqui pode-se fazer calculo para as variaveis
+    document = Document("MEMORIAL.docx")
+    dictionary = {
+        #Aqui as palavras serão substituidas por variaveis da parte da gráfica.
+        "MEMORIAL":"CACETA"
+    }
+
+    for p in document.paragraphs:
+        for key, word in dictionary.items():
+            if p.text.find(key) >= 0:
+                p.text = p.text.replace(key, word)
+
+    document.save("CACETA.docx")
+    print('Deu tudo certo')
+
+editword()
