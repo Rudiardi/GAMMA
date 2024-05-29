4f0f60727c8fa91ecadd9af91dee9c52b5b5c4ae
Rudiardi <rudi.junior@hotmail.com>
2024-05-28 21:54:25 -0300

    Primeiro teste - OK

--- ProgramaSolar.py
+++ ProgramaSolar.py
@@ -1,4 +1,4 @@
 # IMPORTAR AS BIBLIOTECAS
 from tkinter import * # biblioteca gráfica TKinter pip install tk
 from tkinter import ttk
-from docx import Document
+from docx import Document #biblioteca que importa documentos
