9aa9117a624f43add34e15f1cea33e8dd875d034
matheusfmr <franca-matheus@hotmail.com>
2024-05-31 11:13:11 -0300

    função substitui img

--- programasolarMain.py
+++ programasolarMain.py
@@ -2,6 +2,7 @@
 from tkinter import * # biblioteca gráfica TKinter pip install tk
 from tkinter import ttk
 from docx import Document  #biblioteca que importa documentos  pip install python-docx
+from docx.shared import Inches
 from openpyxl import load_workbook #Biblioteca para abrir o excel  pip install openpyxl
 
 #Criação de dicionário para a substituição das variaveis
@@ -22,3 +23,44 @@ def editword():
     print('Deu tudo certo')
 
 editword()
+
+def editimg():
+    # Carregue o arquivo Word existente
+    document = Document("MEMORIAL.docx")
+
+    # Itere pelos parágrafos e substitua a imagem com a tag "replace_me"
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    for run in paragraph.runs:
+                        if 'Fig1' in run.text:
+                            # Substitua pelo caminho da nova imagem
+                            for existing_run in paragraph.runs:
+                                paragraph._element.remove(existing_run._element)
+                            # Adicione a nova imagem (substitua pelo caminho correto)
+                            paragraph.add_run().add_picture('fig1.png',width=Inches(5))
+                        if 'Fig2' in run.text:
+                            # Substitua pelo caminho da nova imagem
+                            for existing_run in paragraph.runs:
+                                paragraph._element.remove(existing_run._element)
+                            # Adicione a nova imagem (substitua pelo caminho correto)
+                            paragraph.add_run().add_picture('fig2.png',width=Inches(4))
+                        if 'Fig3' in run.text:
+                            # Substitua pelo caminho da nova imagem
+                            for existing_run in paragraph.runs:
+                                paragraph._element.remove(existing_run._element)
+                            # Adicione a nova imagem (substitua pelo caminho correto)
+                            paragraph.add_run().add_picture('fig3.png',width=Inches(4))
+                        if 'Fig4' in run.text:
+                            # Substitua pelo caminho da nova imagem
+                            for existing_run in paragraph.runs:
+                                paragraph._element.remove(existing_run._element)
+                            # Adicione a nova imagem (substitua pelo caminho correto)
+                            paragraph.add_run().add_picture('fig4.png',width=Inches(3))    
+
+    # Salve o documento modificado
+    document.save("CACETA.docx")
+    print("imagem subistituida")
+
+editimg()
